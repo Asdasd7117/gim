f2070ebbcb4d7a6062a560ae0ed9230f674418eb
Asdasd7117 <alywm100@gmail.com>
2025-04-23 03:03:39 +0300

    Create ad.s.js

---
+++ ad.s.js
@@ -0,0 +1,7 @@
+(function (propeller) {
+  var script = document.createElement('script');
+  script.src = 'https://propu.sh/pfe/current/tag.min.js?z=9244556';
+  script.async = true;
+  script.setAttribute('data-cfasync', 'false');
+  document.body.appendChild(script);
+})();
